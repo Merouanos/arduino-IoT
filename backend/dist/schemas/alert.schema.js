43fce4460fedbef665af7117288756514509fc53
Merouanos <77001281+Merouanos@users.noreply.github.com>
2026-09-06 23:08:37 +0100

    Delete backend/dist directory

--- backend/dist/schemas/alert.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createAlertSchema = void 0;
-const zod_1 = require("zod");
-exports.createAlertSchema = zod_1.z.object({
-    type: zod_1.z.string().min(1),
-    severity: zod_1.z.string().min(1),
-    message: zod_1.z.string().min(1),
-});
